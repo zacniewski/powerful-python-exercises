c6e318cb2790036c1f255777b503d6543b2775ba
Artur Zacniewski <a.zacniewski@gmail.com>
2022-05-20 19:54:12 +0200

    Plain Python files instead Jupyter

---
+++ Ch2 - Scaling_With_Generators.py
@@ -0,0 +1,33 @@
+### Iteration with iter()
+print("Powerful Python has just started!")
+
+numbers = [7 , 4 , 11 , 3]
+numbers_iter = iter(numbers)
+
+print(f"{list(numbers_iter)=}")
+
+print(f"{id(numbers)=}")
+
+print(f"{id(numbers_iter)=}")
+
+print(f"{numbers.__iter__=}")
+
+print(f"{numbers_iter.__iter__=}")
+
+names = ["Tom", "Shelly", "Garth"]
+names_iter = iter(names)
+print(f"{next(names_iter)=}")
+
+print(f"{next(names_iter)=}")
+
+print(f"Squares numbers with generator")
+
+
+def gen_squares(max_num: int):
+    for num in range(max_num):
+        yield num ** 2
+
+
+MAX = 50
+for square in gen_squares(MAX):
+    print(f"{square=}")
