e1ebfd89ee9992726b62dcb1fac65fd8c1dac2ad
Artur Zacniewski <a.zacniewski@gmail.com>
2022-06-05 21:15:31 +0200

    @printlog decorator

--- Ch5-Decorators.py
+++ Ch5-Decorators.py
@@ -12,3 +12,19 @@ class Person:
 
 person = Person("John", "Doe")
 print(person.full_name)
+
+
+# Ex 2
+def printlog(func):
+    def wrapper(*args, **kwargs):
+        print(f"CALLING {func.__name__}")
+        return func(*args, **kwargs)
+    return wrapper
+
+
+@printlog
+def foo(x):
+    print(x + 2)
+
+
+foo(8)
